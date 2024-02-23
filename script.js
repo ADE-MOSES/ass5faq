b542950ddf87b479603f62dc9cb5b1ed4bb1ac35
ADE-MOSES <157116705+ADE-MOSES@users.noreply.github.com>
2024-02-23 10:40:23 +0100

    adding Faq file

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleFAQ(faqNumber) {
+    const answer = document.getElementById(`faq${faqNumber}`);
+    answer.style.display = (answer.style.display === 'block') ? 'none' : 'block';
+  }
+  
+  
+  
+  
